49da9d450e6d6fdaf675e701cc14745428941e88
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-18 16:04:45 +0200

    add route

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+})
+
 app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
